74eef2cd2b2a840da9474f4d25014b813edccfbe
JokerD <56380626+JokerDKha@users.noreply.github.com>
2020-07-21 15:06:03 +0700

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -1,1 +1,1 @@
-SosfmtMsgBin.exe extract "C:\Users\khajo\Desktop\SOSFoMT FILE\SosfmtMsgBin\file bin here" "C:\Users\khajo\Desktop\SOSFoMT FILE\SosfmtMsgBin\file xml here"
+SosfmtMsgBin.exe extract "%cd%\file bin here" "%cd%\file xml here"
