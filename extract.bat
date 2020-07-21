6b474622f38f6e136b7a1957dc2a9849c4ed6ada
JokerD <56380626+JokerDKha@users.noreply.github.com>
2020-07-21 14:23:16 +0700

    Add files via upload

---
+++ extract.bat
@@ -0,0 +1,1 @@
+SosfmtMsgBin.exe extract "C:\Users\khajo\Desktop\SOSFoMT FILE\SosfmtMsgBin\file bin here" "C:\Users\khajo\Desktop\SOSFoMT FILE\SosfmtMsgBin\file xml here"
---
+++ pack.bat
@@ -0,0 +1,1 @@
+SosfmtMsgBin.exe pack "C:\Users\khajo\Desktop\SOSFoMT FILE\SosfmtMsgBin\file xml here" "C:\Users\khajo\Desktop\SOSFoMT FILE\SosfmtMsgBin\file bin here" 
